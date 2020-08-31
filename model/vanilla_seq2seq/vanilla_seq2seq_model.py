209041428ffad70b9887d5164fb8a76d95509d6e
Ignacio Pastor <54788305+IpastorSan@users.noreply.github.com>
2020-08-31 17:46:51 +0200

    Update vanilla_seq2seq_model.py

--- model/vanilla_seq2seq/vanilla_seq2seq_model.py
+++ model/vanilla_seq2seq/vanilla_seq2seq_model.py
@@ -280,7 +280,7 @@ def translate_sentence(input_sentence, target_tokenizer, decoding_dict, output_m
         output_token, decoder_states = decoder_inf_model.predict([target_sequence,  states_value])
         id_word = np.argmax(output_token[0, -1, :])
 
-        if id_word == end_of_sequence or input_sentence[0][i] == 0:  #this is wrong so far
+        if id_word == end_of_sequence or len(translated_sentence) == output_max_len:
             break
 
         decoded_word = ""
